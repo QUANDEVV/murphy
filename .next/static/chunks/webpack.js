23b989a22bbc92520763385e095a441a5f6fed1e
QUANDEVV <vercelploy@gmail.com>
2024-08-09 10:30:48 +0300

    done

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fafe40c98ecdf7d1"; }
+/******/ 		__webpack_require__.h = function() { return "0b584852c8789aae"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
